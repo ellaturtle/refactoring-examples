c648768ab95a94ddabe25fd99829c7d13e6c7266
Ella Turtle <ucaptur@ucl.ac.uk>
2025-12-01 10:49:02 +0000

    Adding more variable names

--- boids/boids.py
+++ boids/boids.py
@@ -21,34 +21,38 @@ boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(50)]
 boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
 
 # Create a function to update the position and velocity of the boids
-def update_boids(boids):
-    global boids_x, boids_y, boid_x_velocities, boid_y_velocities
-    xs,ys,xvs,yvs= boids_x, boids_y, boid_x_velocities, boid_y_velocities
-    boids_number = len(xs)
+def update_boids(boids_tuple):
+    positions_x, positions_y, velocities_x, velocities_y = boids_tuple
+    boids_number = len(positions_x)
+    dx = positions_x[neighbor_boid] - positions_x[current_boid]
+    dy = positions_y[neighbor_boid] - positions_y[current_boid]
 
     # Fly towards the middle
     for current_boid in range(boids_number):
         for neighbor_boid in range(boids_number):
-            xvs[current_boid]=xvs[current_boid]+(xs[neighbor_boid]-xs[current_boid])*0.01/boids_number
+            velocities_x[current_boid]=velocities_x[current_boid]+(dx)*0.01/boids_number
+
     for current_boid in range(boids_number):
         for neighbor_boid in range(boids_number):
-            yvs[current_boid]=yvs[current_boid]+(ys[neighbor_boid]-ys[current_boid])*0.01/boids_number
+            velocities_y[current_boid]=velocities_y[current_boid]+(dy)*0.01/boids_number
 
     # Fly away from nearby boids
     for current_boid in range(boids_number):
         for neighbor_boid in range(boids_number):
-            if (xs[neighbor_boid]-xs[current_boid])**2 + (ys[neighbor_boid]-ys[current_boid])**2 < 100:
-                xvs[current_boid]=xvs[current_boid]+(xs[current_boid]-xs[neighbor_boid])
-                yvs[current_boid]=yvs[current_boid]+(ys[current_boid]-ys[neighbor_boid])
+            if (dx)**2 + (dy)**2 < 100:
+                velocities_x[current_boid] += (positions_x[current_boid]-positions_x[neighbor_boid])
+                velocities_y[current_boid] += (positions_y[current_boid]-positions_y[neighbor_boid])
 
     # Try to match speed with nearby boids
     for current_boid in range(boids_number):
         for neighbor_boid in range(boids_number):
-            if (xs[neighbor_boid]-xs[current_boid])**2 + (ys[neighbor_boid]-ys[current_boid])**2 < 10000:
-                xvs[current_boid]=xvs[current_boid]+(xvs[neighbor_boid]-xvs[current_boid])*0.125/boids_number
-                yvs[current_boid]=yvs[current_boid]+(yvs[neighbor_boid]-yvs[current_boid])*0.125/boids_number
+            if (dx)**2 + (dy)**2 < 10000:
+                velocities_x[current_boid] += (velocities_x[neighbor_boid]-velocities_x[current_boid])*0.125/boids_number
+                velocities_y[current_boid] += (velocities_y[neighbor_boid]-velocities_y[current_boid])*0.125/boids_number
 
     # Move according to velocities
     for current_boid in range(boids_number):
-        xs[current_boid]=xs[current_boid]+xvs[current_boid]
-        ys[current_boid]=ys[current_boid]+yvs[current_boid]
+        positions_x[current_boid] += velocities_x[current_boid]
+        positions_y[current_boid] += velocities_y[current_boid]
+
+    return(positions_x, positions_y, velocities_x, velocities_y)
